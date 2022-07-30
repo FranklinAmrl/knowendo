3998ce4d0de5d046eb7b005f440dd872c0090b0e
Franklin <franklinamaralt@gmail.com>
2022-07-30 18:10:38 -0300

    installed and configured the app and settgins.py

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'config',
     'bootstrap4',
-    'chartjs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
