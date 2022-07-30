7c61a576e21a314067d294090b25f42cd91fd3b2
Franklin <franklinamaralt@gmail.com>
2022-07-30 18:18:22 -0300

    changed the db

--- config/settings.py
+++ config/settings.py
@@ -78,17 +78,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'knowendo',
-
-        'USER': 'postgres',
-
-        'PASSWORD': '123123',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
